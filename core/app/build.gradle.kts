b7692863f9bb4baa22269c249c25aecdbc0ee2c5
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-24 15:23:37 +0100

    ui: enable Compose for AndroidIDE Pro shell

--- core/app/build.gradle.kts
+++ core/app/build.gradle.kts
@@ -91,6 +91,13 @@ dependencies {
   kapt(projects.annotation.processors)
 
   implementation(libs.common.editor)
+
+  // Compose Pro UI
+  implementation(libs.compose.runtime)
+  implementation(libs.compose.ui)
+  implementation(libs.compose.ui.tooling.preview)
+  implementation(libs.compose.foundation)
+  implementation(libs.compose.material3)
   implementation(libs.common.utilcode)
   implementation(libs.common.glide)
   implementation(libs.common.jsoup)

2daee1e2e679a524627fd0aff4b0a7817395bda9
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-24 16:06:35 +0100

    build(native): embed Kotlin compiler in native engine

--- core/android-build/build.gradle.kts
+++ core/android-build/build.gradle.kts
@@ -3,9 +3,15 @@ plugins {
   id("org.jetbrains.kotlin.jvm")
 }
 
+kotlin {
+  jvmToolchain(17)
+}
+
 dependencies {
   api(projects.core.buildApi)
   implementation(projects.core.buildEngine)
+  implementation(projects.core.languageSupport)
+  implementation(libs.kotlin.compiler.embeddable)
 }
 
 description = "Native Android APK build pipeline"

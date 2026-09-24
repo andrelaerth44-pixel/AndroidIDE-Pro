43f48f940d79fd2bd06532f12ffcd72443209e36
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-24 15:15:02 +0100

    build: add native Android build engine v1

---
+++ core/android-build/src/main/kotlin/com/itsaky/androidide/build/android/AndroidModel.kt
@@ -0,0 +1,52 @@
+package com.itsaky.androidide.build.android
+
+import java.nio.file.Path
+
+data class AndroidSdk(
+  val root: Path,
+  val buildToolsVersion: String,
+  val compileSdk: Int
+) {
+  val buildTools: Path
+    get() = root.resolve("build-tools").resolve(buildToolsVersion)
+
+  val platform: Path
+    get() = root.resolve("platforms").resolve("android-" + compileSdk)
+
+  val aapt2: Path get() = buildTools.resolve("aapt2")
+  val d8: Path get() = buildTools.resolve("d8")
+  val zipalign: Path get() = buildTools.resolve("zipalign")
+  val apksigner: Path get() = buildTools.resolve("apksigner")
+
+  fun androidJar(): Path = platform.resolve("android.jar")
+}
+
+data class AndroidModule(
+  val name: String,
+  val rootDir: Path,
+  val namespace: String,
+  val applicationId: String,
+  val compileSdk: Int,
+  val minSdk: Int,
+  val targetSdk: Int,
+  val sdk: AndroidSdk
+) {
+  val manifest: Path get() = rootDir.resolve("src/main/AndroidManifest.xml")
+  val sourceDir: Path get() = rootDir.resolve("src/main/java")
+  val resourceDir: Path get() = rootDir.resolve("src/main/res")
+  val assetDir: Path get() = rootDir.resolve("src/main/assets")
+
+  val buildDir: Path get() = rootDir.resolve("build/androidide")
+  val mergedResourcesDir: Path get() = buildDir.resolve("intermediates/merged-res/debug")
+  val compiledResourcesDir: Path get() = buildDir.resolve("intermediates/compiled-res/debug")
+  val generatedDir: Path get() = buildDir.resolve("generated/debug")
+  val generatedRDir: Path get() = generatedDir.resolve("r")
+  val generatedBuildConfigDir: Path = generatedDir.resolve("buildConfig")
+  val classesDir: Path get() = buildDir.resolve("intermediates/classes/debug")
+  val dexDir: Path get() = buildDir.resolve("intermediates/dex/debug")
+  val resourcesApk: Path get() = buildDir.resolve("outputs/resources.ap_")
+  val unsignedApk: Path get() = buildDir.resolve("outputs/app-debug-unsigned.apk")
+  val alignedApk: Path get() = buildDir.resolve("outputs/app-debug-aligned.apk")
+  val signedApk: Path get() =
+    rootDir.resolve("build/outputs/apk/debug/" + name + "-debug.apk")
+}

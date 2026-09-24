193eec9d44b62963246bc7ef481f9f92108de87f
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-24 20:01:28 +0100

    build(android-build): remove embedded Kotlin compiler

--- core/android-build/build.gradle.kts
+++ core/android-build/build.gradle.kts
@@ -7,20 +7,11 @@ kotlin {
   jvmToolchain(17)
 }
 
-val kotlinToolchain by configurations.creating
-
 dependencies {
   api(projects.core.buildApi)
   implementation(projects.core.buildEngine)
   implementation(projects.core.languageSupport)
-  implementation(libs.kotlin.compiler.embeddable)
   implementation(libs.composite.javac)
-  kotlinToolchain(libs.kotlin.compiler.embeddable)
-}
-
-tasks.register<Sync>("packageKotlinToolchain") {
-  from(kotlinToolchain)
-  into(layout.buildDirectory.dir("toolchains/kotlin"))
 }
 
 description = "Native Android APK build pipeline"

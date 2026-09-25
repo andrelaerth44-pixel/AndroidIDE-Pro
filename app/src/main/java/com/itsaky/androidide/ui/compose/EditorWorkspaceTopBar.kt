404c96bbb092eb2ee50fa23d27947985e4c5d1bb
andrelaerth44-pixel <andrelaerth44@gmail.com>
2026-09-25 08:09:26 +0100

    fix: finish workspace build and tab visuals

--- app/src/main/java/com/itsaky/androidide/ui/compose/EditorWorkspaceTopBar.kt
+++ app/src/main/java/com/itsaky/androidide/ui/compose/EditorWorkspaceTopBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
